b9c595b562dc1fbdad2e097bb134a34786a1a544
Saivardhan16 <saivardhanjindam@gmail.com>
2026-06-19 22:45:20 +0530

    Add files via upload

---
+++ PhonePe Wallet Backend/src/main/java/org/phonepe/phonepaywallet/dto/TransactionResponse.java
@@ -0,0 +1,23 @@
+package org.phonepe.phonepaywallet.dto;
+
+import org.phonepe.phonepaywallet.entity.Transaction.TransactionStatus;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+
+public class TransactionResponse {
+    private Long transactionId;
+    private String senderUpi;
+    private String receiverUpi;
+    private BigDecimal amount;
+    private LocalDateTime date;
+    private TransactionStatus status;
+
+}
